db84d21dd5c7023e3a1c71700ac4d02714115edd
Lucas Bourdon <lucas.bourdon2002@hotmail.fr>
2022-04-29 11:18:26 +0200

    Menu Victoire / Défaite

---
+++ CelticDruid/Assets/Menu/Menu_Défaite.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Défaite : MonoBehaviour
+{
+    void Update()
+    {
+        
+        if(Input.GetAxis("Vertical") > 0)
+        {
+
+            SceneManager.LoadScene("Cernunnos_game");
+        }
+        
+        if(Input.GetAxis("Vertical") < 0)
+        {
+            Application.Quit();
+            Debug.Log("quit");
+
+        }
+    }
+
+}
